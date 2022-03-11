6346702601236eeaafb30f75ffae19bff8e50139
Fibinger Ádám <adam.fibinger@wup.hu>
2022-03-11 20:15:22 +0100

    initial commit

---
+++ apps/iroda/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'joladnijo-backoffice-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'iroda';
+}
